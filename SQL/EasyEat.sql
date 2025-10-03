d394d39785a7a4e38788ecbedafb41da5c9178ce
Nguyen Quoc Minh <thnrgbefv0987@gmail.com>
2025-10-03 21:55:56 +0700

    feat: finished basic skeleton of 8 tables #5

--- SQL/EasyEat.sql
+++ SQL/EasyEat.sql
@@ -1,47 +1,134 @@
--- Table: public.users
+-- Drops
 
+DROP TABLE IF EXISTS public.product;
+DROP TABLE IF EXISTS public.category;
+DROP TABLE IF EXISTS public.seller;
 DROP TABLE IF EXISTS public.admin;
 DROP TABLE IF EXISTS public.customer;
 DROP TABLE IF EXISTS public.users;
+DROP TABLE IF EXISTS public.role;
+
+-- Table: role
+
+CREATE TABLE IF NOT EXISTS public.role
+(
+	role_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 CACHE 1 ),
+    role_name character varying(32) NOT NULL,
+    CONSTRAINT role_pk PRIMARY KEY (role_id)
+);
+
+ALTER TABLE IF EXISTS public.role
+    OWNER to postgres;
+
+INSERT INTO public.role (role_name) VALUES ('guest');
+INSERT INTO public.role (role_name) VALUES ('customer');
+INSERT INTO public.role (role_name) VALUES ('seller');
+INSERT INTO public.role (role_name) VALUES ('admin');
+
+-- Table: users
 
 CREATE TABLE IF NOT EXISTS public.users
 (
     user_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100000 MINVALUE 100000 MAXVALUE 999999 CACHE 1 ),
     email character varying(254) COMPRESSION lz4 COLLATE pg_catalog."default" UNIQUE,
+	role_id bigint,
 	username character varying(64),
 	password_hash character varying(64),
 	is_active bit NOT NULL,
     creation_date date,
-    CONSTRAINT user_pk PRIMARY KEY (user_id)
+    CONSTRAINT user_pk PRIMARY KEY (user_id),
+	CONSTRAINT user_fk_role FOREIGN KEY (role_id) 
+		REFERENCES public.role (role_id)
 );
 
 ALTER TABLE IF EXISTS public.users
     OWNER to postgres;
 
--- Table: public.customer
+-- Table: customer
 
 CREATE TABLE IF NOT EXISTS public.customer
 (
-    user_id bigint NOT NULL,
-    CONSTRAINT customer_pk PRIMARY KEY (user_id),
+	customer_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100000 MINVALUE 100000 MAXVALUE 999999 CACHE 1 ),
+    user_id bigint NOT NULL UNIQUE,
+    CONSTRAINT customer_pk PRIMARY KEY (customer_id),
     CONSTRAINT customer_fk_user FOREIGN KEY (user_id)
         REFERENCES public.users (user_id)
-		ON DELETE CASCADE
 );
 
 ALTER TABLE IF EXISTS public.customer
     OWNER to postgres;
 
--- Table: public.admin
+-- Table: admin
 
 CREATE TABLE IF NOT EXISTS public.admin
 (
-    user_id bigint NOT NULL,
-    CONSTRAINT admin_pk PRIMARY KEY (user_id),
+	admin_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100000 MINVALUE 100000 MAXVALUE 999999 CACHE 1 ),
+    user_id bigint NOT NULL UNIQUE,
+    CONSTRAINT admin_pk PRIMARY KEY (admin_id),
     CONSTRAINT admin_fk_user FOREIGN KEY (user_id) 
         REFERENCES public.users (user_id)
-		ON DELETE CASCADE
 );
 
 ALTER TABLE IF EXISTS public.admin
+    OWNER to postgres;
+
+-- Table: seller
+
+CREATE TABLE IF NOT EXISTS public.seller
+(
+	seller_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100000 MINVALUE 100000 MAXVALUE 999999 CACHE 1 ),
+    user_id bigint NOT NULL UNIQUE,
+    CONSTRAINT seller_pk PRIMARY KEY (seller_id),
+    CONSTRAINT seller_fk_user FOREIGN KEY (user_id) 
+        REFERENCES public.users (user_id)
+);
+
+ALTER TABLE IF EXISTS public.seller
+    OWNER to postgres;
+
+-- Table: category
+
+CREATE TABLE IF NOT EXISTS public.category
+(
+	category_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 CACHE 1 ),
+    category_name bigint NOT NULL UNIQUE,
+    CONSTRAINT category_pk PRIMARY KEY (category_id)
+);
+
+ALTER TABLE IF EXISTS public.category
+    OWNER to postgres;
+
+-- Table: product
+
+CREATE TABLE IF NOT EXISTS public.product
+(
+	product_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 100000 MINVALUE 100000 MAXVALUE 999999 CACHE 1 ),
+    seller_id bigint NOT NULL,
+	category_id bigint NOT NULL,
+    CONSTRAINT product_pk PRIMARY KEY (product_id),
+    CONSTRAINT product_fk_seller FOREIGN KEY (seller_id) 
+        REFERENCES public.seller (seller_id),
+	CONSTRAINT product_fk_category FOREIGN KEY (category_id) 
+        REFERENCES public.category (category_id)
+);
+
+ALTER TABLE IF EXISTS public.product
+    OWNER to postgres;
+
+-- Table: cart
+
+CREATE TABLE IF NOT EXISTS public.cart
+(
+	user_id bigint NOT NULL,
+	product_id bigint NOT NULL,
+    amount bigint,
+	date_added date,
+    CONSTRAINT cart_pk PRIMARY KEY (user_id, product_id),
+    CONSTRAINT cart_fk_user FOREIGN KEY (user_id) 
+        REFERENCES public.users (user_id),
+	CONSTRAINT cart_fk_product FOREIGN KEY (product_id) 
+        REFERENCES public.product (product_id)
+);
+
+ALTER TABLE IF EXISTS public.cart
     OWNER to postgres;
