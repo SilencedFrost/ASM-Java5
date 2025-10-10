d358efc3c94aae365f82be7dcb4f3a7ccfb6faa2
nguyenlenganha150207 <nguyenlenganha923@gmail.com>
2025-10-10 20:28:11 +0700

    fix #39'
    git commit -m database

--- ASM2_API/ASM-Java5/src/main/java/com/entity/Role.java
+++ ASM2_API/ASM-Java5/src/main/java/com/entity/Role.java
@@ -16,7 +16,7 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "role_id", nullable = false)
-    private Integer roleId;
+    private int roleId;
 
     @Column(name = "role_name", nullable = false, length = 32)
     private String roleName;
--- ASM2_API/ASM-Java5/src/main/java/com/entity/Seller.java
+++ ASM2_API/ASM-Java5/src/main/java/com/entity/Seller.java
@@ -13,11 +13,11 @@ public class Seller {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "seller_id", nullable = false)
-    private long sellerId;
-
-    @Column(name = "user_id", nullable = false, unique = true)
-    private long userId;
+//    @Column(name = "seller_id", nullable = false)
+//    private long sellerId;
+//
+//    @Column(name = "user_id", nullable = false, unique = true)
+//    private long userId;
 
     @Column(name = "shop_name", length = 64)
     private String shopName;
