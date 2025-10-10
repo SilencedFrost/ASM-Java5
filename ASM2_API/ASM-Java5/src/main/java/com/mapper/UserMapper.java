368805693351fa95f82b003aa31b6a7317f389f8
Nguyen Quoc Minh <thnrgbefv0987@gmail.com>
2025-10-10 23:47:20 +0700

    feat: added user, session mappers #45

--- ASM2_API/ASM-Java5/src/main/java/com/mapper/UserMapper.java
+++ ASM2_API/ASM-Java5/src/main/java/com/mapper/UserMapper.java
@@ -1,43 +1,15 @@
 package com.mapper;
 
-import com.dto.InboundUserDTO;
-import com.dto.OutboundUserDTO;
-import com.dto.UserDTO;
-import com.entity.Role;
+import com.dto.user.UserCreateRequest;
+import com.dto.user.UserResponse;
 import com.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserResponse toDTO(User user);
 
-public class UserMapper {
-
-    public static OutboundUserDTO toDTO(User user) {
-        if (user == null) {
-            return null;
-        }
-        return new OutboundUserDTO(
-                user.getUserId(),
-                user.getUsername(),
-                user.getEmail(),
-                user.getCreationDate(),
-                user.getRole() != null ? user.getRole().getRoleName() : "null",
-                user.isActive(),
-                user.getLastLoginDate()
-        );
-    }
-
-    public static User toEntity(InboundUserDTO userDTO, Role role) {
-        if (userDTO == null) {
-            return null;
-        }
-
-        return new User(
-                userDTO.getUsername(),
-                userDTO.getEmail(),
-                userDTO.getPasswordHash(),
-                role,
-                userDTO.isActive()
-        );
-    }
+    @Mapping(target = "userId", ignore = true)
+    User toEntity(UserCreateRequest userRequest);
 }
