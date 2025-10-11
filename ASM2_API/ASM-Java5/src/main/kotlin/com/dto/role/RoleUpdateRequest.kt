203f7f28f99a20d153d6fe688380fb0d13e65ccc
SilencedFrost <thnrgbefv0987@gmail.com>
2025-10-11 11:19:54 +0700

    feat: removed kotlin dtos, switched to java record #41

--- ASM2_API/ASM-Java5/src/main/kotlin/com/dto/role/RoleUpdateRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.dto.role
-
-data class RoleUpdateRequest(
-    val roleId: Int,
-    val roleName: String
-)
--- ASM2_API/ASM-Java5/src/main/kotlin/com/dto/session/SessionCreateRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.dto.session
-
-data class SessionCreateRequest(
-    val userId: Long,
-    val sessionToken: String,
-    val userAgent: String
-)
