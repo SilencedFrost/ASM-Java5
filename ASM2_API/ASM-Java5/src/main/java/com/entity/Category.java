920e50a42dd239631049195a295c55e393ced80b
Phuc Nguyen <trongphuc2313@gmail.com>
2025-10-12 12:24:54 +0700

    refactor: Refine Product and Category entity mappings

--- ASM2_API/ASM-Java5/src/main/java/com/entity/Category.java
+++ ASM2_API/ASM-Java5/src/main/java/com/entity/Category.java
@@ -7,9 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "category")
+@Table(name = "category", schema = "public")
 @Getter
-@Setter
 @NoArgsConstructor
 public class Category {
 
@@ -18,17 +17,20 @@ public class Category {
     @Column(name = "category_id")
     private int categoryId;
 
+    @Setter
     @Column(name = "category_name", length = 64, nullable = false, unique = true)
     private String categoryName;
 
+    @Setter
     @Column(name = "description", length = 256)
     private String description;
 
+    @Setter
     @Column(name = "is_active")
     private Boolean isActive;
 
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = false)
-    private List<Product> products = new ArrayList<>();
+//    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = false)
+//    private List<Product> products = new ArrayList<>();
 
     public Category(String categoryName) {
         this.categoryName = categoryName;
