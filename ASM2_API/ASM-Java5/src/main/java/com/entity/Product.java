e9c3053f87d4630f3be2f732ae6bf1d514ecf6de
Phuc Nguyen <trongphuc2313@gmail.com>
2025-10-12 12:24:54 +0700

    refactor: Refine Product and Category entity mappings

--- ASM2_API/ASM-Java5/src/main/java/com/entity/Product.java
+++ ASM2_API/ASM-Java5/src/main/java/com/entity/Product.java
@@ -11,9 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "product")
+@Table(name = "product", schema = "public")
 @Getter
-@Setter
 @NoArgsConstructor
 public class Product {
 
@@ -22,38 +21,47 @@ public class Product {
     @Column(name = "product_id")
     private Long productId;
 
+    @Setter
     @Column(name = "product_name", nullable = false, length = 128)
     private String productName;
 
-    @Column(name = "Price", nullable = false, precision = 15, scale = 2)
+    @Setter
+    @Column(name = "price", nullable = false, precision = 15, scale = 2)
     private BigDecimal price;
 
+    @Setter
     @Column(name = "stock_count", nullable = false)
-    private int stockQuantity;
+    private int stockCount;
 
+    @Setter
     @Column(name = "is_active")
-    private Boolean active;
+    private Boolean isActive;
 
-    @Lob
+    @Setter
     @Column(name = "description", nullable = false, columnDefinition = "text")
-    private String productDescription;
+    private String description;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id")
-    private Category category;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "category_id")
+//    private Category category;
 
+    @Setter
     @Column(name = "thumbnail_extension", length = 5)
     private String thumbnailExtension;
 
+    @Setter
     @Column(name = "product_size", length = 32)
     private String productSize;
 
+    @Setter
     @Column(name = "variation", length = 32)
     private String variation;
 
+    @Setter
     @Column(name = "view_count")
     private int viewCount;
 
+    @Setter
     @Column(name = "total_sales")
     private int totalSales;
 
@@ -65,21 +73,21 @@ public class Product {
     @Column(name = "updated_at")
     private OffsetDateTime updateDate;
 
-    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Cart> carts = new ArrayList<>();
+//    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Cart> carts = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Comment> comments = new ArrayList<>();
 
-    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Comment> comments = new ArrayList<>();
-
-    public void setCategory(Category category) {
-        if (this.category != null) {
-            this.category.getProducts().remove(this);
-        }
-
-        this.category = category;
-
-        if (category != null) {
-            category.getProducts().add(this);
-        }
-    }
+//    public void setCategory(Category category) {
+//        if (this.category != null) {
+//            this.category.getProducts().remove(this);
+//        }
+//
+//        this.category = category;
+//
+//        if (category != null) {
+//            category.getProducts().add(this);
+//        }
+//    }
 }
