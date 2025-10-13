200b2d2f0d96045bd75bc29ddce475d698c349a5
WinG <nguyendailong2809@gmail.com>
2025-10-13 17:00:17 +0700

    feat: Mapper and Repo: Address, city #64

---
+++ ASM2_API/ASM-Java5/src/main/java/com/mapper/AddressMapper.java
@@ -0,0 +1,19 @@
+package com.mapper;
+
+import com.dto.address.AddressCreateRequest;
+import com.dto.address.AddressResponse;
+import com.entity.Address;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        componentModel = "spring",
+        unmappedSourcePolicy = ReportingPolicy.IGNORE
+)
+public interface AddressMapper {
+    AddressResponse toDTO(Address adress);
+
+    Address toEntity(AddressCreateRequest addressCreateRequest);
+}
+
