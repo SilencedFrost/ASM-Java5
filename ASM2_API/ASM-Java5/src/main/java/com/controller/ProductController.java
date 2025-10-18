ddc6589fff69911c63ef0e04c313d103d922cf77
Nguyen Quoc Minh <thnrgbefv0987@gmail.com>
2025-10-18 08:59:46 +0700

    fix: removed unecessary import 2

--- ASM2_API/ASM-Java5/src/main/java/com/controller/ProductController.java
+++ ASM2_API/ASM-Java5/src/main/java/com/controller/ProductController.java
@@ -1,7 +1,6 @@
 package com.controller;
 
 import com.dto.product.ProductResponse;
-import com.entity.Product;
 import com.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
